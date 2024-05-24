b6bab4a6c7e306b5adbb7a2d095097e1b7a98ca6
Gh61 <T.Zenkner@email.cz>
2024-05-24 14:18:00 +0200

    feat: add admin privileges elevation

---
+++ Commands/RestartAsAdminCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Windows;
+
+namespace Gh61.EdgePdfPreviewEnabler.Commands
+{
+    internal class RestartAsAdminCommand : UICommandBase<object>
+    {
+        public static RestartAsAdminCommand Instance { get; } = new RestartAsAdminCommand();
+
+        private RestartAsAdminCommand() : base("Restart as Administrator")
+        {
+        }
+
+        protected override void Execute(object parameter)
+        {
+            var exePath = Assembly.GetExecutingAssembly().Location;
+
+            var proc = new Process()
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = exePath,
+                    UseShellExecute = true,
+                    Verb = "runas"
+                }
+            };
+
+            bool isElevated = false;
+            try
+            {
+                proc.Start();
+                isElevated = true;
+            }
+            catch (Exception)
+            {
+                // user canceled the elevation prompt
+            }
+
+            if (isElevated)
+            {
+                // close current application
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
